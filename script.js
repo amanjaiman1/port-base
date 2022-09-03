a8336f7787241aa504cc13bec017a2693e428c01
AMAN JAIMAN <77478827+amanjaiman1@users.noreply.github.com>
2022-09-04 00:24:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,101 @@
+window.addEventListener("DOMContentLoaded",() => {
+	const clock = new BouncyBlockClock(".clock");
+});
+
+class BouncyBlockClock {
+	constructor(qs) {
+		this.el = document.querySelector(qs);
+		this.time = { a: [], b: [] };
+		this.rollClass = "clock__block--bounce";
+		this.digitsTimeout = null;
+		this.rollTimeout = null;
+		this.mod = 0 * 60 * 1000;
+
+		this.loop();
+	}
+	animateDigits() {
+		const groups = this.el.querySelectorAll("[data-time-group]");
+
+		Array.from(groups).forEach((group,i) => {
+			const { a, b } = this.time;
+
+			if (a[i] !== b[i]) group.classList.add(this.rollClass);
+		});
+
+		clearTimeout(this.rollTimeout);
+		this.rollTimeout = setTimeout(this.removeAnimations.bind(this),900);
+	}
+	displayTime() {
+		// screen reader time
+		const timeDigits = [...this.time.b];
+		const ap = timeDigits.pop();
+
+		this.el.ariaLabel = `${timeDigits.join(":")} ${ap}`;
+
+		// displayed time
+		Object.keys(this.time).forEach(letter => {
+			const letterEls = this.el.querySelectorAll(`[data-time="${letter}"]`);
+
+			Array.from(letterEls).forEach((el,i) => {
+				el.textContent = this.time[letter][i];
+			});
+		});
+	}
+	loop() {
+		this.updateTime();
+		this.displayTime();
+		this.animateDigits();
+		this.tick();
+	}
+	removeAnimations() {
+		const groups = this.el.querySelectorAll("[data-time-group]");
+
+		Array.from(groups).forEach(group => {
+			group.classList.remove(this.rollClass);
+		});
+	}
+	tick() {
+		clearTimeout(this.digitsTimeout);
+		this.digitsTimeout = setTimeout(this.loop.bind(this),1e3);
+	}
+	updateTime() {
+		const rawDate = new Date();
+		const date = new Date(Math.ceil(rawDate.getTime() / 1e3) * 1e3 + this.mod);
+		let h = date.getHours();
+		const m = date.getMinutes();
+		const s = date.getSeconds();
+		const ap = h < 12 ? "AM" : "PM";
+
+		if (h === 0) h = 12;
+		if (h > 12) h -= 12;
+
+		this.time.a = [...this.time.b];
+		this.time.b = [
+			(h < 10 ? `0${h}` : `${h}`),
+			(m < 10 ? `0${m}` : `${m}`),
+			(s < 10 ? `0${s}` : `${s}`),
+			ap
+		];
+
+		if (!this.time.a.length) this.time.a = [...this.time.b];
+	}
+}
+
+
+
+document.querySelector('svg').innerHTML = [...Array(11)]
+    .map((_, i) => Math.pow(Math.sqrt(2), i))
+    .map((s, i) => `
+        <rect transform="scale(${s})rotate(${i*45})"
+            stroke-width="${2/s}"
+            stroke="hsl(${i*30},55%,55%)"
+            width="10" height="10" x="-5" y="-5"></rect>
+    `).join(',');
+
+
+    document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
